9b4ff5346badd45f64b764d4ed45dfc7032bdabd
AlanYen <alanyen.mobile@gmail.com>
2015-11-17 14:01:50 +0800

    Initial Commit

---
+++ DemoParseServer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  DemoParseServer
+//
+//  Created by AlanYen on 2015/11/17.
+//  Copyright © 2015年 17Life. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
